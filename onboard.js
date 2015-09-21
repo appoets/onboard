c14933515aa57281df2418ffa60136e0bb9192c6
ayokesh <ayokesh.cse@gmail.com>
2015-09-21 15:50:18 +0530

    added bootstrap and initial commit

--- onboard.js
+++ onboard.js
@@ -2,18 +2,14 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+  Router.route('/', function () {
+      this.route('home', {
+    name: 'home',
+    path: '/',
+    template: 'views/home'
+    });
+});
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
 }
 
 if (Meteor.isServer) {
